56b4963d770e2fb2f27af1b31e78316381c08bc1
Miska-Mynttinen <miska.mynttinen@aalto.fi>
2022-06-11 14:10:38 +0300

    puhelinluettelo versio 2.7 eli step 2

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
 const Person = (props) => {
- return (
-  <p>
-    {props.name}
-  </p>
- )
+  return (
+    <p>
+      {props.name}
+    </p>
+  )
 }
 
 const App = () => {
@@ -19,14 +19,20 @@ const App = () => {
     const personObject = {
       name: newName
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    if (duplicateName) {
+      window.alert(`${newName} is alreade added to phonebook`)
+      return
+    }
+      setPersons(persons.concat(personObject))
+      setNewName('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const duplicateName = (persons, name) => persons.map(person => person.name).includes(name)
 
   return (
     <div>
@@ -44,7 +50,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
         {persons.map(person => 
-          <Person key={person.name} name={person.name} />)}
+          <Person key={person.name} name={person.name}/>)}
     </div>
   )
 
