6a7befa6ab7519c47521497d0832d65a41c63dd1
Miska-Mynttinen <miska.mynttinen@aalto.fi>
2022-07-25 21:36:04 +0300

    4.20 blogilistan laajennus

--- osa4/blogilista_backend/controllers/blogs.js
+++ osa4/blogilista_backend/controllers/blogs.js
@@ -5,13 +5,6 @@ require('dotenv').config()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -40,9 +33,8 @@ blogsRouter.post('/', async (request, response) => {
     })
   }
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
   const user = await User.findById(decodedToken.id)
