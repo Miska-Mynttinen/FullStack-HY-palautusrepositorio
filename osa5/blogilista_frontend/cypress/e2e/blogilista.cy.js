9005580f2ab9c9a2e409c59b4cb40459f38493da
Miska-Mynttinen <miska.mynttinen@aalto.fi>
2023-05-19 17:52:38 +0300

    5.18 blogilistan end2end testi kirjautumisen onnistumiselle ja epäonnitumiselle

--- osa5/blogilista_frontend/cypress/e2e/blogilista.cy.js
+++ osa5/blogilista_frontend/cypress/e2e/blogilista.cy.js
@@ -1,6 +1,14 @@
+const user = {
+  name: 'tester user',
+  username: 'tester',
+  password: '1234'
+}
+
+
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3003')
   })
 
@@ -9,5 +17,25 @@ describe('Blog app', function() {
     cy.get('#username')
     cy.get('#password')
     cy.get('#login-button')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('tester')
+      cy.get('#password').type('1234')
+      cy.get('#login-button').click()
+
+      cy.contains('tester user logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('login').click()
+      cy.get('#username').type('tester')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password')
+    })
   })
 })
