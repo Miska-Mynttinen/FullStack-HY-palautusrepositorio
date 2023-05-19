ea527514b2e75015fa1b859f8b9bb7c32048dbb0
Miska-Mynttinen <miska.mynttinen@aalto.fi>
2023-05-19 17:12:58 +0300

    5.17 cypress configuraation ja testi login formista etusivulla

---
+++ osa5/blogilista_frontend/cypress/e2e/blogilista.cy.js
@@ -0,0 +1,13 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3003')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('login').click()
+    cy.get('#username')
+    cy.get('#password')
+    cy.get('#login-button')
+  })
+})
