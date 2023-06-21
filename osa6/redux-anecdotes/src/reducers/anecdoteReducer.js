39dc35cb0f380c4d53968b989240638bd2965672
Miska-Mynttinen <miska.mynttinen@aalto.fi>
2023-06-21 21:27:25 +0300

    6.17 anekdootit ja backend, step 4

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -28,12 +28,19 @@ const anecdoteSlice = createSlice({
   }
 })
 
-export const { voteFor, createAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const { voteFor, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
   return async dispatch => {
     const anecdotes = await anecdoteService.getAll()
     dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const createAnecdote = content => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
   }
 }
 
