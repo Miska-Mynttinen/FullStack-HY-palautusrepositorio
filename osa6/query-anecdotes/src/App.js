0213cb8d057538ce9b6f25d66cfa8c5840299355
Miska-Mynttinen <miska.mynttinen@aalto.fi>
2023-06-22 22:13:54 +0300

    6.20 & 6.21, react query

--- osa6/query-anecdotes/src/App.js
+++ osa6/query-anecdotes/src/App.js
@@ -1,26 +1,49 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes, createAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+    },
+  })
+
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  }
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery('anecdotes', getAnecdotes, { refetchOnWindowFocus: false })
+  console.log(result)
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if ( result.isError ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+  console.log('anecdotes', anecdotes)
 
   return (
     <div>
       <h3>Anecdote app</h3>
     
       <Notification />
-      <AnecdoteForm />
+      <AnecdoteForm addAnecdote={addAnecdote}/>
     
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
